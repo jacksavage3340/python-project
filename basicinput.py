b87909829f110a04554fbb81ea14dbac93d4387c
Pious Jacob <133435846+jacksavage3340@users.noreply.github.com>
2024-10-18 15:57:54 +0530

    Add files via upload

--- basicinput.py
+++ basicinput.py
@@ -18,4 +18,17 @@ str1="Hello, World!"
 str2=" Everyone!"
 str3=str1[7:12]+str2
 print(str3)
+#task2
 
+"""Author: Pious Jacob
+Date: 18/10/2024
+Write a Python program that demonstrates the usage of arithmetic, comparison, and logical operators.
+Perform a few operations and print the results."""
+a=int(input("Enter 1st no:"))
+b=int(input("Enter 2nd no:"))
+print("Sum:",a+b,"Division:",a/b)
+print("Is a greater than b?:",a>b)
+print("Are a and b equal?:",a==b)
+print("Logical AND:",a>0 and b>0)
+print("Logical OR:",a>0 or b>10)
+#taks3
