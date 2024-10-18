0ff41ca44168747703bd705eebd7afc6a4afacb6
Pious Jacob <133435846+jacksavage3340@users.noreply.github.com>
2024-10-18 14:29:56 +0530

    python basic input

---
+++ basicinput.py
@@ -0,0 +1,14 @@
+"""Author: Pious Jacob
+Date: 18/10/2024
+Write a Python program that asks the user to input their name and age. After receiving the input,
+display a message that includes the user's name and age.
+Expected Output:
+Enter your name: John
+Enter your age: 25
+Hello, John! You are 25 years old."""
+n=input("Enter your name:")
+a=int(input("Enter your age:"))
+print("Hello",n+"! You are",a,"years old.")
+
+
+
