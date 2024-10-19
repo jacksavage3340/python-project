3a1130eddf163d4afbcfc94d48b5040a862a6085
Pious Jacob <133435846+jacksavage3340@users.noreply.github.com>
2024-10-19 15:09:43 +0530

    Add files via upload

--- task 22.py
+++ task 22.py
@@ -16,6 +16,7 @@ first_name=input("Enter your first name:")
 last_name=input("Enter your last name")
 name=first_name+" "+last_name
 print(name)
+print(len(first_name))
 first_name_length=len(first_name)
 extracted_last_name=name[first_name_length+1:]
 print(extracted_last_name)
